c3199038cb64903edb1687943b013270d9098fad
trevino <rafael.trevino@me.com>
2018-12-18 19:02:27 -0500

    cleaned up code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
 var tweetArray = [];
-var tweetText = $
 
 function getTweets() {
     var jsonUrl = "https://rafaeltrevino.github.io/trumptranslator/assets/javascript/master_2018.json";
@@ -9,14 +8,11 @@ function getTweets() {
         url: jsonUrl,
         method: "GET",
     }).then(function(data) {
-        console.log(data);
-        console.log(data.length);
         for (i=0; i < data.length; i++) {
             var tweetDate = data[i].created_at;
             var tweet = data[i].text;
             tweetArray.push([tweetDate, tweet]);
         }
-        console.log(tweetArray);
         var d = $("<div>");
         var p = $("<p>");
         var randTweet = tweetArray[Math.floor(Math.random() * tweetArray.length)];
@@ -28,36 +24,28 @@ function getTweets() {
     });
 };
 
-
-console.log(tweetText);
-
 function displayGif(tweetText) {
     var APIkey = "b98xRER1URXt0Nhz68BEVXWnfI43okvO";
     var queryURL = "https://api.giphy.com/v1/gifs/translate?s=" + tweetText + "&api_key=" + APIkey + "&weirdness=1"
-    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function(response) {
-        console.log(response);
-        console.log(response.data);
-        // for (i=0; i < response.data.length; i++) {
-            var gifDivCol = $("<div>").addClass("col-md");
-            var p = $("<p>");
-            var img = $("<img>");
-            var altText = $(response.data.title);
-            var imgSrc = response.data.images.fixed_height.url;
-            var imgStill = response.data.images.fixed_height_still.url;
-            var imgAnimate = response.data.images.fixed_height.url;
-            img.attr("src", imgSrc).attr("alt", altText);
-            img.attr("data-id", response.data.id);
-            img.attr("data-still", imgStill);
-            img.attr("data-animate", imgAnimate);
-            p.append(img);
-            console.log(p);
-            gifDivCol.append(p);
-            $("#gifDiv").prepend(gifDivCol);
-        //}
+        var gifDivCol = $("<div>").addClass("col-md");
+        var p = $("<p>");
+        var img = $("<img>");
+        var altText = $(response.data.title);
+        var imgSrc = response.data.images.fixed_height.url;
+        var imgStill = response.data.images.fixed_height_still.url;
+        var imgAnimate = response.data.images.fixed_height.url;
+        img.attr("src", imgSrc).attr("alt", altText);
+        img.attr("data-id", response.data.id);
+        img.attr("data-still", imgStill);
+        img.attr("data-animate", imgAnimate);
+        p.append(img);
+        console.log(p);
+        gifDivCol.append(p);
+        $("#gifDiv").prepend(gifDivCol);
     });
 }
 
